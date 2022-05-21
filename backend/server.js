44a09303e7ad257c131ba40fb92963b62960a527
noahlissner <noah.lissner@gmail.com>
2022-05-21 22:51:54 +0200

    created register + login API

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => {
+  console.log("Server listening to port");
+});
