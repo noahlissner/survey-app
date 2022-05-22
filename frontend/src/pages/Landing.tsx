ee21f7606287d391f34d63db88aa0fbc48a18f4f
noahlissner <noah.lissner@gmail.com>
2022-05-22 22:09:06 +0200

    added darkmode hook + switcher

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,5 +1,12 @@
+import Switcher from "../components/Switcher";
+
 const Landing = () => {
-	return <div>Landing</div>;
+  return (
+    <div className="bg-white dark:bg-black h-screen">
+      <Switcher />
+      <h1 className="text-7xl mt-11 text-black dark:text-white">Landing</h1>
+    </div>
+  );
 };
 
 export default Landing;
