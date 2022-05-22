e023b7fdbdfcb113f7d39dd6ae80ff028aa75761
dough <viktor.melin@me.com>
2022-05-22 23:03:21 +0200

    Mobile(first)

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,32 +1,16 @@
 import { FiLogIn } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import Button from '../components/Button';
+import Navigation from '../components/Navigation';
 
 const Landing = () => {
 	return (
-		<div className='relative overflow-hidden h-screen w-screen bg-gradient-to-r from-blue-100 to-blue-300 flex justify-center'>
-			<nav className='fixed w-full flex justify-between items-center py-5 px-10'>
-				<div className='flex items-center gap-5'>
-					<img
-						className='mr-10'
-						src='http://acmelogos.com/images/logo-7.svg'
-						alt='Logo'
-					/>
-					<Link className='font-medium' to='/'>
-						Home
-					</Link>
-				</div>
-				<div className='flex items-center'>
-					<Link className='font-medium flex items-center gap-2' to='/login'>
-						<FiLogIn /> Login
-					</Link>
-				</div>
-			</nav>
-			<main className='h-full w-9/12 flex justify-center flex-col'>
-				<h1 className='font-semibold text-6xl mb-7'>
+		<div className='relative overflow-hidden h-screen w-screen bg-gradient-to-r from-blue-100 to-blue-300 dark:from-blue-700 dark:to-blue-800 flex justify-center'>
+			<Navigation />
+			<main className='h-full p-10 lg:p-0 lg:w-9/12 flex items-center lg:items-start justify-center flex-col'>
+				<h1 className='font-semibold text-2xl place-self-start mb-5 lg:text-6xl lg:mb-7 dark:text-white'>
 					Company Name<span className='text-blue-400 text-6xl'>.</span>
 				</h1>
-				<p className='w-1/2'>
+				<p className='lg:w-1/2 dark:text-white'>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
 					placeat repellat voluptatum, aut quia incidunt facere, eum sint quidem
 					doloribus quod hic deserunt tempore mollitia impedit dicta ducimus,
@@ -40,7 +24,7 @@ const Landing = () => {
 				</Link>
 			</main>
 			<img
-				className='absolute h-[70rem] bottom-[-10rem] right-[-10rem] pointer-events-none'
+				className='hidden absolute h-[40rem] bottom-[-10rem] right-[-10rem] pointer-events-none lg:block xl:h-[50rem]'
 				src='https://imagedelivery.net/3ecvmLCFkS-FijMWb0qFvQ/521d5abb-c4fb-460e-a18e-b91bcc4a8500/public'
 				alt=''
 			/>
