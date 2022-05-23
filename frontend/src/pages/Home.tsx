6ef8c02c734c21747da7f32efb5c98f28c169b91
dough <viktor.melin@me.com>
2022-05-23 23:21:43 +0200

    Initial dashboard layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,55 +1,54 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { IoNotificationsOutline, IoPersonCircle } from "react-icons/io5";
-import Sidebar from "../containers/Sidebar";
-import Switcher from "../components/Switcher";
-import DashboardTopNav from "../components/DashboardTopNav";
-import Settings from "../containers/Settings";
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoNotificationsOutline, IoPersonCircle } from 'react-icons/io5';
+import Sidebar from '../containers/Sidebar';
+import Switcher from '../components/Switcher';
+import DashboardTopNav from '../components/DashboardTopNav';
+import Settings from '../containers/Settings';
 
 // Redux
-import { useSelector, useDispatch } from "react-redux";
-import { logout, reset } from "../features/auth/authSlice";
-import { AppDispatch, RootState } from "../app/store";
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
+import { AppDispatch, RootState } from '../app/store';
+import Dashboard from '../containers/Dashboard';
 
 const Home = () => {
-  const { user } = useSelector((state: RootState) => state.auth);
-  const currentPage = useSelector((state: RootState) => state.page.value);
+	const { user } = useSelector((state: RootState) => state.auth);
+	const currentPage = useSelector((state: RootState) => state.page.value);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+	const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    if (!user) {
-      navigate("/");
-    }
-  });
+	useEffect(() => {
+		if (!user) {
+			navigate('/');
+		}
+	});
 
-  const onLogout = () => {
-    dispatch(logout());
-    dispatch(reset());
-    navigate("/");
-  };
+	const onLogout = () => {
+		dispatch(logout());
+		dispatch(reset());
+		navigate('/');
+	};
 
-  return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <main className="flex-1 flex flex-col items-center bg-gradient-to-r from-blue-100 to-blue-300 dark:from-blue-700 dark:to-blue-800">
-        <DashboardTopNav />
-        <section className="flex-1 w-full">
-          {currentPage === "Dashboard" && (
-            <h1 className="text-6xl text-white">Dashboard</h1>
-          )}
-          {currentPage === "Surveys" && (
-            <h1 className="text-6xl text-white">Surveys</h1>
-          )}
-          {currentPage === "Quizes" && (
-            <h1 className="text-6xl text-white">Quizes</h1>
-          )}
-          {currentPage === "Settings" && <Settings />}
-        </section>
-      </main>
-      {/* <button onClick={onLogout}>Logout</button> */}
-    </div>
-  );
+	return (
+		<div className='flex h-screen'>
+			<Sidebar />
+			<main className='flex-1 flex flex-col items-center bg-gradient-to-r from-blue-100 to-blue-300 dark:from-blue-700 dark:to-blue-800'>
+				<DashboardTopNav />
+				<section className='flex-1 w-full'>
+					{currentPage === 'Dashboard' && <Dashboard />}
+					{currentPage === 'Surveys' && (
+						<h1 className='text-6xl text-white'>Surveys</h1>
+					)}
+					{currentPage === 'Quizes' && (
+						<h1 className='text-6xl text-white'>Quizes</h1>
+					)}
+					{currentPage === 'Settings' && <Settings />}
+				</section>
+			</main>
+			{/* <button onClick={onLogout}>Logout</button> */}
+		</div>
+	);
 };
 export default Home;
