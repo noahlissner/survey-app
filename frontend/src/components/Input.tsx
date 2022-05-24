792a65ec2ddd030cec7078a716a302363ba89ce5
noahlissner <noah.lissner@gmail.com>
2022-05-24 21:34:56 +0200

    Remove styles for auto-complete

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -18,12 +18,12 @@ const Input: React.FC<Props> = ({
   icon,
 }) => {
   return (
-    <div className="relative w-full bg-blue-300 dark:bg-blue-800 rounded-[20px] flex items-center">
+    <div className="overflow-hidden relative w-full bg-blue-300 dark:bg-blue-800 rounded-[20px] flex items-center">
       <input
         type={type}
         id={name}
         name={name}
-        className="bg-transparent block px-4 pb-3 pt-6 w-full focus:outline-none focus:border-blue-600 peer text-lg dark:text-white"
+        className="autofill:bg-yellow-300 bg-transparent block px-4 pb-3 pt-6 w-full focus:outline-none focus:border-blue-600 peer text-lg dark:text-white"
         placeholder=" "
         onChange={onChange}
         value={value}
