35f4c376d16b4efdef5ee88e0f492130b4cc932c
dough <viktor.melin@me.com>
2022-06-05 00:34:17 +0200

    Dashboard + Edit Password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,11 +5,13 @@ const {
 	registerUser,
 	loginUser,
 	editUser,
+	editPassword,
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.put('/edit', protect, editUser);
+router.put('/editpassword', protect, editPassword);
 
 module.exports = router;
