a0e0cb4ccb00f50cd8ae074577e97e256d00071e
dough <viktor.melin@me.com>
2022-06-06 00:23:10 +0200

    Fixed token

--- frontend/src/services/surveyApi.ts
+++ frontend/src/services/surveyApi.ts
@@ -4,7 +4,7 @@ const API_URL = '/api/surveys/';
 export const newSurvey = async (surveyData: any) => {
 	const config = {
 		headers: {
-			authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOWQxMTA5OWQ4ZjNjZmNkZWYyZDZkMSIsImlhdCI6MTY1NDQ2MDY4MSwiZXhwIjoxNjU3MDUyNjgxfQ.NYcrwB-dUOQPNYc2XzosHD4VWyaR0tApz1ekd8Z0-BM`,
+			authorization: `Bearer`,
 		},
 	};
 
