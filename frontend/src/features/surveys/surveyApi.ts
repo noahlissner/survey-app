9af52a43a93f46ccca0ce5cc11e032f85c8db7b3
Noah <noah.lissner@gmail.com>
2022-06-07 09:00:14 +0200

    send token to backend when creating new survey

--- frontend/src/features/surveys/surveyApi.ts
+++ frontend/src/features/surveys/surveyApi.ts
@@ -1,31 +1,35 @@
-import axios from 'axios';
-const API_URL = '/api/surveys/';
+import axios from "axios";
+const API_URL = "/api/surveys/";
+
+const user = JSON.parse(localStorage.getItem("user") || "");
+const token = user.token;
+console.log(user.token);
 
 const newSurvey = async (surveyData: any) => {
-	const config = {
-		headers: {
-			authorization: `Bearer`,
-		},
-	};
+  const config = {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.post(API_URL + 'new', surveyData, config);
-	return response.data;
+  const response = await axios.post(API_URL + "new", surveyData, config);
+  return response.data;
 };
 
 const fetchSurveys = async (_id: string, token: string) => {
-	const config = {
-		headers: {
-			authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.get(API_URL + 'fetch', config);
-	return response.data;
+  const response = await axios.get(API_URL + "fetch", config);
+  return response.data;
 };
 
 const surveyService = {
-	newSurvey,
-	fetchSurveys,
+  newSurvey,
+  fetchSurveys,
 };
 
 export default surveyService;
