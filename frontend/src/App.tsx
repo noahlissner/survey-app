f53dc9cc38bf508c64e7e746af9f462260b44cb4
noahlissner <noah.lissner@gmail.com>
2022-06-17 21:17:09 +0200

    Fixed conflict in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
@@ -8,6 +7,7 @@ import Surveys from "./containers/Surveys";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import SurveySlug from "./pages/SurveySlug";
 
 const App: React.FC = () => {
   return (
@@ -19,32 +19,13 @@ const App: React.FC = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="/surveys" element={<Surveys />} />
+        <Route path="/surveys/:slug" element={<SurveySlug />} />
         <Route path="/quizes" element={<></>} />
         <Route path="/settings" element={<Settings />} />
       </Route>
       <Route path="*" element={<p>Error</p>} />
     </Routes>
   );
-=======
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import SurveySlug from './pages/SurveySlug';
-
-const App: React.FC = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<Landing />} />
-			<Route path='/dashboard' element={<Home />} />
-			<Route path='/register' element={<Register />} />
-			<Route path='/login' element={<Login />} />
-			<Route path='/surveys/:slug' element={<SurveySlug />} />
-		</Routes>
-	);
->>>>>>> bf3f92ded177f3f5304922affe07e4fb4e7e21c4
 };
 
 export default App;
